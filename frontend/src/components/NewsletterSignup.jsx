763b39a73529bf8661197e5a38b0df9af368d01f
Dmytro <deekorde@gmail.com>
2024-04-11 10:36:25 +0300

    newsletter page and componenet completed

--- frontend/src/components/NewsletterSignup.jsx
+++ frontend/src/components/NewsletterSignup.jsx
@@ -1,15 +1,30 @@
+import { useFetcher } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import classes from './NewsletterSignup.module.css';
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const inputRef = useRef();
+
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === 'idle' && data && data.message) {
+      window.alert(data.message)
+      inputRef.current.value = '';
+    }
+  }, [data, state])
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form method="post" action='/newsletter' className={classes.newsletter}>
       <input
+      ref={inputRef}
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
