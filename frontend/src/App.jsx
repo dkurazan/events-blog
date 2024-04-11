dd777b537f37aebff818c6feb0ee2b0595a12919
Dmytro <deekorde@gmail.com>
2024-04-11 10:58:19 +0300

    events defering added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     {
         path: "/",
         element: <RootPage />,
-        errorElement: <ErrorPage />,
+        // errorElement: <ErrorPage />,
         children: [
             {
                 index: true,
